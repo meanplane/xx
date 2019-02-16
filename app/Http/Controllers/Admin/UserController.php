34800bf2b41177845e0b583e370c70288f6b80a1
mplane <185878205@qq.com>
2019-02-16 18:16:54 +0800

    no message

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -8,11 +8,13 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Support\Facades\Validator;
 
-class UserController extends Controller {
-
-    public function lists(){
-        if(!request()->ajax()){
+class UserController extends Controller
+{
+    public function lists()
+    {
+        if (!request()->ajax()) {
             return $this->view();
         }
 
@@ -28,40 +30,91 @@ class UserController extends Controller {
             $where[] = ['status', request('status')];
         }
 
-        $res = m('admin.user')->getLists($where,request('limit'),request('page'));
+        $res = m('admin.user')->getLists($where, request('limit'), request('page'));
+        $res['roles'] = m('admin.group')->pluck('name', 'id')->toArray();
+        $res['levels'] = [1 => '普通', 2 => '主管', 3 => '经理'];
 
-        return $this->success('',$res);
+        return $this->success('', $res);
     }
 
-    // 修改自己信息
-    public function updateInfo(){
-        if(!request()->ajax()){
-            $userInfo = request()->user('admin')->toArray();
-            return view('admin.updateInfo',compact('userInfo'));
-        }
-
-        $id = request('id');
-        $params = request(['mobile','realname','email']);
-        m('admin.user')->where('id',$id)->update($params);
-
-        return $this->success('修改成功');
+    public function add()
+    {
+        return $this->storage();
     }
 
-    // 修改自己密码
-    public function updatePass(){
-        if(!request()->ajax()){
-            $userInfo = request()->user('admin')->toArray();
-            return view('admin.updatePass',compact('userInfo'));
+    public function edit()
+    {
+        return $this->storage();
+    }
+
+    private function storage()
+    {
+        $m = m('admin.user');
+        $data = request()->all();
+        unset($data['groups']);
+        if ($id = request('id')) {
+            $rs = $m->where('id', $id)->update($data);
+            $admin_id = $id;
+        } else {
+            //添加
+            if ($m->where('name', $data['name'])->value('id')) {
+                return $this->error('用户已存在');
+            }
+
+            // 自定义 验证消息
+            $validator = Validator::make(request()->all(), $m->rules, $m->messages);
+            if ($validator->fails()) {
+                return ($this->error($validator->errors()));
+            }
+
+            $data['password'] = bcrypt($data['password']);
+            $rs = $m->create($data);
+            $admin_id = $rs->id;
         }
 
-        $id = request('id');
-        $password = request('password');
-
-        if(empty($password) || strlen($password)<4){
-            return $this->error('新密码不能为空,且必须大于 4 位!');
+        if (request('groups')) {
+            m('admin.groupAccess')->store($admin_id, request('groups'));
+        } else {
+            m('admin.groupAccess')->del($admin_id);
         }
 
-        m('admin.user')->where('id',$id)->update(['password'=> bcrypt(request('password'))]);
-        return $this->success('修改成功');
+        if ($rs) {
+            return $this->success('添加成功');
+        } else {
+            return $this->error();
+        }
+    }
+
+    public function del()
+    {
+        $res = m('admin.user')->where('id', request('id'))->delete();
+        if ($res) {
+            return $this->success('删除成功!');
+        }
+        return $this->error();
+    }
+
+    public function changePwd()
+    {
+        $info = m('admin.user')->find(request('id'));
+
+        if (!$info) {
+            return $this->error('非法请求');
+        }
+
+        $validator = Validator::make(request()->all(), [
+            'password' => 'required|min:3|max:20',
+        ], [
+            'password.required' => '密码不能为空',
+        ]);
+        if ($validator->fails()) {
+            return $this->error($validator->errors());
+        }
+
+        $res = m('admin.user')->where('id', request('id'))->update(['password' => bcrypt(request('password'))]);
+        if ($res) {
+            return $this->success('修改成功');
+        }
+        return $this->error();
     }
 }
--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -1,10 +1,8 @@
-
 <div class="right-content" style=" width: 100%;height: 100%;padding: 20px;box-sizing: border-box;" id="content">
     <div class="title"
          style="line-height: 40px;border-bottom: 1px solid #c1c1c1;padding-bottom: 20px;margin-bottom:30px;">
         <h3 style="line-height: 1;color: deepskyblue">{{$menu_info->name or ''}}</h3>
     </div>
-
 
     <el-card shadow="hover" style="margin:auto 30px;">
         <div slot="header" class="clearfix">
@@ -12,7 +10,7 @@
                 <el-form-item label="账号名">
                     <el-input v-model="searchData.name" placeholder="菜单名"></el-input>
                 </el-form-item>
-                <el-form-item label="状态" >
+                <el-form-item label="状态">
                     <el-select v-model="searchData.status">
                         <el-option label="全部" value=""></el-option>
                         <el-option label="正常" value="1"></el-option>
@@ -22,8 +20,8 @@
                 <el-form-item>
                     <el-button type="primary" @click="onSearch">查询</el-button>
                     <el-button @click="refreshSearch">重置查询</el-button>
+                    <el-button @click="showAdd" type="primary" style="margin-left:60px;">新增管理员</el-button>
                 </el-form-item>
-
             </el-form>
         </div>
 
@@ -37,47 +35,64 @@
             </el-table-column>
             <el-table-column
                     prop="name"
-                    label="账号"
-                    width="150">
+                    label="账号">
             </el-table-column>
             <el-table-column
                     label="真名"
-                    prop="realname"
-                    width="100">
+                    prop="realname">
             </el-table-column>
             <el-table-column
                     prop="level"
                     label="级别"
+                    :formatter="(row)=>levels[row.level] ">
+            </el-table-column>
+            <el-table-column
+                    :formatter="(row)=>showGroups(row)"
+                    label="角色">
+            </el-table-column>
+            <el-table-column
+                    label="状态">
+                <template slot-scope="scope">
+                    <el-tag v-if="scope.row.status == 1" type="success" size="mini">正常</el-tag>
+                    <el-tag v-else type="danger" size="mini">已禁用</el-tag>
+                </template>
+            </el-table-column>
+            <el-table-column
+                    label="创建时间"
+                    :formatter="(row)=>formatTime(row.created_at)"
                     width="100">
             </el-table-column>
             <el-table-column
-                    prop="groups"
-                    label="角色"
-                    width="200">
+                    label="修改时间"
+                    :formatter="(row)=>formatTime(row.updated_at)"
+                    width="100">
             </el-table-column>
-            {{--<el-table-column--}}
-                    {{--label="创建时间"--}}
-                    {{--:formatter="(row)=>formatTime(row.created_at)"--}}
-                    {{--width="200">--}}
-            {{--</el-table-column>--}}
-            {{--<el-table-column--}}
-                    {{--label="修改时间"--}}
-                    {{--:formatter="(row)=>formatTime(row.updated_at)"--}}
-                    {{--width="200">--}}
-            {{--</el-table-column>--}}
             <el-table-column
                     prop="href"
-                    label="地址">
+                    label="操作"
+                    align="center"
+                    width="280">
                 <template slot-scope="scope">
-                    <el-button
-                            size="mini"
-                            @click="showInfo(scope.row)">编辑</el-button>
-                    <el-button
-                            size="mini"
-                            @click="showInfo(scope.row)">修改密码</el-button>
-                    <el-button
-                            size="mini"
-                            @click="showInfo(scope.row)">禁用</el-button>
+                    <el-button-group>
+                        <el-button
+                                size="mini" plain
+                                @click="showEdit(scope.row)">编辑
+                        </el-button>
+                        <el-button
+                                size="mini" plain
+                                @click="showEditPass(scope.row)">修改密码
+                        </el-button>
+                        <el-button v-if="scope.row.status == 1" size="mini" type="danger" plain
+                                @click="disableUser(scope.row.id,2)">禁用
+                        </el-button>
+                        <el-button v-else size="mini" type="success" plain
+                                   @click="disableUser(scope.row.id,1)">解禁
+                        </el-button>
+                    </el-button-group>
+
+                    <el-button size="mini" type="danger"
+                               @click="delUser(scope.row.id)">删除
+                    </el-button>
                 </template>
             </el-table-column>
         </el-table>
@@ -94,31 +109,95 @@
         </el-pagination>
     </el-card>
 
-    <el-dialog title="" :visible.sync="showInfoDialog" center width="1000">
+    {{-- 添加用户 --}}
+    <el-dialog :title="editTitle" :visible.sync="showEditDialog" center width="1000">
+        <el-form label-width="100px" style="margin-right:30px;margin-bottom:50px;">
+            <el-form-item label="账号">
+                <el-input v-model="editData.name"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" >
+                <el-input v-model="editData.password" :disabled="showPass"></el-input>
+            </el-form-item>
+            <el-form-item label="真实姓名">
+                <el-input v-model="editData.realname"></el-input>
+            </el-form-item>
+            <el-form-item label="手机号">
+                <el-input v-model="editData.mobile"></el-input>
+            </el-form-item>
+            <el-form-item label="级别">
+                <el-select v-model="editData.level">
+                    <el-option v-for="(name,key) in levels" :label="name" :value="Number(key)"></el-option>
+                </el-select>
+            </el-form-item>
 
+            <el-form-item label="角色">
+                <el-checkbox-group v-model="editData.groups">
+                    <el-checkbox v-for="(name,key) in roles" :label="Number(key)" :key="Number(key)">@{{name}}</el-checkbox>
+                </el-checkbox-group>
+            </el-form-item>
+            <el-form-item label="状态">
+                <el-select v-model="editData.status">
+                    <el-option v-for="(name,key) in statuss" :label="name" :value="Number(key)"></el-option>
+                </el-select>
+            </el-form-item>
+        </el-form>
+        <div style="text-align: center">
+            <el-button type="primary" @click="onSubmit">确定</el-button>
+        </div>
+    </el-dialog>
+
+    <el-dialog title="修改密码" :visible.sync="showEditPassDialog" center width="500">
+        <el-form label-width="100px" style="margin-right:30px;margin-bottom:50px;">
+            <el-form-item label="新密码" >
+                <el-input v-model.trim="editPass.password"></el-input>
+            </el-form-item>
+        </el-form>
+        <div style="text-align: center">
+            <el-button type="primary" @click="onEditPass">确定</el-button>
+        </div>
     </el-dialog>
 
 </div>
 
 <script>
+    var _editData = {
+        name: '',
+        password: '',
+        realname: '',
+        mobile: '',
+        level: 1,
+        groups: [],
+        status: 1
+    };
 
     new Vue({
         el: '#content',
         data: function () {
             return {
+                levels: {},
+                statuss: {1:'正常',2:'禁用'},
                 tableData: [],
-                total:0,
-                searchData:{
-                    name:'',
-                    status:'',
+                total: 0,
+                searchData: {
+                    name: '',
+                    status: '',
 
-                    limit:10,
-                    page:1
+                    limit: 10,
+                    page: 1
                 },
 
-                showInfoDialog:false,
-                info:{},
-                last_info:{}
+                roles: {},
+
+                showEditDialog: false,
+                editTitle: '',
+                editData: deepCopy(_editData),
+                showPass:true,
+
+                showEditPassDialog: false,
+                editPass: {
+                    id:'',
+                    password:'',
+                }
             }
         },
         methods: {
@@ -130,18 +209,10 @@
                 this.searchData.page = val;
                 this._getData();
             },
-            showInfo(row){
-//                console.log(row)
-//                this.showInfoDialog = true;
-//                ajaxPost(this,'/admin/log/info',{id:row.id},null,(res)=>{
-//                    console.log(res)
-//                })
+            onSearch() {
+                this._getData();
             },
-            onSearch(){
-//                this._getData();
-                console.log(this.searchData);
-            },
-            refreshSearch(){
+            refreshSearch() {
                 this.searchData.name = '';
                 this.searchData.status = '';
 
@@ -150,18 +221,95 @@
 
                 this._getData();
             },
-            _getData(){
-                ajaxPost(this, '/admin/user/lists', this.searchData , null, (res) => {
+            _getData() {
+                ajaxPost(this, '/admin/user/lists', this.searchData, null, (res) => {
                     this.tableData = res.tableData;
+                    this.roles = res.roles;
+                    this.levels = res.levels;
                     this.total = res.count;
                 })
             },
-            formatTime(row){
+            formatTime(row) {
                 return moment(row * 1000).format('YYYY-MM-DD HH:mm:ss')
+            },
+            showGroups (row){
+                if(!row.groups){
+                    return '';
+                }
+                str = '';
+                for(r of row.groups){
+                    str += this.roles[r] + ' , ';
+                }
+                return str.slice(0,-3);
+            },
+
+            showAdd() {
+                this.showEditDialog = true;
+                this.showPass = false;
+                this.editTitle = '添加管理员';
+                this.editData = deepCopy(_editData);
+            },
+            showEdit(row) {
+                this.showEditDialog = true;
+                this.showPass = true;
+                this.editTitle = '编辑管理员';
+                for(var k in this.editData){
+                    this.editData[k] = row[k];
+                }
+                delete(this.editData.password);
+                this.editData.id = row.id;
+                this.editData.groups = this.editData.groups || [];
+
+                console.log(this.editData);
+            },
+            onSubmit(){
+                this.showEditDialog = false;
+                console.log(this.editData);
+                if(this.editData.id){
+                    ajaxPost(this, '/admin/user/edit', this.editData, null, () => {
+                        this._getData();
+                    })
+                }else{
+                    ajaxPost(this, '/admin/user/add', this.editData, null, () => {
+                        this._getData();
+                    })
+                }
+            },
+//
+            showEditPass(row) {
+                this.showEditPassDialog = true;
+                this.editPass.id = row.id;
+                this.editPass.password = '';
+            },
+            onEditPass() {
+                if(this.editPass.password.length < 3 || this.editPass.password.length > 8){
+                    this.$message.error('密码应该为 3-8位之间');
+                    return;
+                }
+                this.showEditPassDialog = false;
+                ajaxPost(this,'/admin/user/changePwd',this.editPass)
+            },
+            // 禁用
+            disableUser(id,status) {
+                var msg = (status === 1) ?'确定要解除禁用吗？':'确定要封禁吗？';
+                this.$confirm(msg).then(()=>{
+                    ajaxPost(this,'/admin/user/edit',{id,status},null,()=>{
+                        this._getData();
+                    })
+                })
+            },
+
+            // 删除
+            delUser(id){
+                this.$confirm('确定要删除此账号吗？').then(()=>{
+                    ajaxPost(this,'/admin/user/del',{id},null,()=>{
+                        this._getData();
+                    })
+                })
             }
         },
         created() {
-           this._getData();
+            this._getData();
         }
     })
 
